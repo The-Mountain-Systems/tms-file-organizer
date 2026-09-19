9b92d913255bf818f1e16b6498c2fbf6f769057e
Ayoub Tajana <ayoubtajana@proximus.be>
2026-09-19 20:38:35 +0200

    removed unnecessary function call

--- linux/linux_f_organizer.py
+++ linux/linux_f_organizer.py
@@ -57,7 +57,7 @@ def main():
     while choice != "Q":
         intro.show_banner()                    
         choice = input("Enter your choice: ")  
-        result = str(intro.options(choice))
+        result = intro.options(choice)
 
         if result == '1':
             organize_img()
