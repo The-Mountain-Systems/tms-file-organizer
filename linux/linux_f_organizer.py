ea1d15bae30a2f526090005b7ef6c1e18c399a51
Ayoub Tajana <ayoubtajana@proximus.be>
2026-09-19 20:11:39 +0200

    added line with author and completed the organize_img() function. Now need to add verbose mode

--- linux/linux_f_organizer.py
+++ linux/linux_f_organizer.py
@@ -1,9 +1,34 @@
 #!/bin/python3
+# Program has been made by The Mountain Systems 2026 Copyright
 import shutil,exifread
+from pathlib import Path
 from modules import IntroScreen
 
 def organize_img():
-    pass
+    home = Path.home()
+    img_extensions = {".jpg", ".jpeg", ".png", ".gif", ".heic"}
+
+    images = []
+    for item in home.rglob("*"):
+        if item.is_file() and item.suffix.lower() in img_extensions:
+            images.append(item)
+    # Look into metadata -> Date&Time
+    for img in images:
+        with open(img,'rb') as f:
+            tags = exifread.process_file(f)
+        date_time = tags.get("EXIF DateTimeOrginial")
+        if date_time:
+            date = str(date_time)[:11]
+        # Check if folder for specific date exists
+        if Path(f"~/Pictures/{date}").is_dir():
+            # Copy img/vid to pictures folder
+            shutil.copy(img,f"~/Pictures/{date}")
+        else:
+            # Creating the folder
+            Path(f"~/Pictures/{date}").mkdir()
+            # Copy img/vid to pictures folder
+            shutil.copy(img,f"~/Pictures/{date}")
+            
 
 def organize_docs():
     pass
